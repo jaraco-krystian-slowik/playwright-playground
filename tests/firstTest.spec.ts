a61e6126943b93864d35e2567c84b07d9725be8f
krystianslowik <krystian.slowik@trustedshops.com>
2024-06-11 01:06:52 +0200

    (test) extracting values

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,4 +1,4 @@
-import { test } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:4200");
   await page.getByText("Forms").click();
@@ -89,10 +89,37 @@ test("fill basic form", async ({ page }) => {
   const basicForm = await page
     .locator("nb-card")
     .filter({ hasText: "Basic Form" });
-
+  const passField = basicForm.getByRole("textbox", { name: "Password" });
   await basicForm
     .getByRole("textbox", { name: "Email" })
     .fill("email@email.com");
-  await basicForm.getByRole("textbox", { name: "Password" }).fill("somepass");
+  await passField.fill("somepass");
   await basicForm.getByRole("button").click();
+
+  await expect(passField).toHaveValue("somepass");
+});
+
+test("extracting values", async ({ page }) => {
+  //single text value
+  const basicForm = await page
+    .locator("nb-card")
+    .filter({ hasText: "Basic Form" });
+
+  const buttonText = await basicForm.locator("button").textContent();
+  expect(buttonText).toEqual("Submit");
+
+  //all text values
+  const allRadioButtonsLabels = await page
+    .locator("nb-radio")
+    .allTextContents();
+  expect(allRadioButtonsLabels).toContain("Option 1");
+
+  //input value
+  const emailField = basicForm.getByRole("textbox", { name: "Email" });
+  await emailField.fill("Test@Test.com");
+  const emailValue = await emailField.inputValue();
+  expect(emailValue).toEqual("Test@Test.com");
+
+  const placeholderValue = await emailField.getAttribute("placeholder");
+  expect(placeholderValue).toEqual("Email");
 });
