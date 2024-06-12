729243ec26b996cd47a3de07730fabe88398abc6
krystianslowik <krystian.slowik@trustedshops.com>
2024-06-12 21:52:43 +0200

    (test) iframe drag&drops

---
+++ tests/iFrameDragAndDrop.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from "@playwright/test";
+
+test("drag and drop within an iframe", async ({ page }) => {
+  await page.goto("https://www.globalsqa.com/demo-site/draganddrop/");
+
+  //consent
+  const consentBox = await page.locator(".fc-consent-root");
+  await consentBox.locator("p.fc-button-label").getByText("Consent").click();
+
+  const frame = page
+    .frameLocator('[rel~="iFrame"]')
+    .frameLocator('[title="Photo Manager"]');
+
+  await frame.locator("li", { hasText: "High Tatras 2" }).hover();
+
+  await page.mouse.down();
+  await frame.locator("#trash").hover();
+  await page.mouse.up();
+
+  const verificationLocator = frame.locator("#trash li h5");
+  await expect(verificationLocator).toHaveText([
+    "High Tatras 2",
+    "High Tatras 4",
+  ]);
+});
