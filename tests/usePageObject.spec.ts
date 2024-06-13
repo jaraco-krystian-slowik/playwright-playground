56e2d2132ec15416deb1968547b8ab7d67ee175c
krystianslowik <krystian.slowik@trustedshops.com>
2024-06-13 17:59:02 +0200

    (test) navigation through the menu

---
+++ tests/usePageObject.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from "@playwright/test";
+import { NavigationPage } from "../page-objects/navigationPage";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("http://localhost:4200");
+});
+
+test("navigate to form page", async ({ page }) => {
+  const navigateTo = new NavigationPage(page);
+  await navigateTo.formLayoutsPage();
+  await page.waitForTimeout(400);
+  await navigateTo.datepickerPage();
+  await page.waitForTimeout(400);
+  await navigateTo.smartTablePage();
+  await page.waitForTimeout(400);
+  await navigateTo.toastPage();
+});
